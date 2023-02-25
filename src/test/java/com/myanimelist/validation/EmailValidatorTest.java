04fb375fc463f0bb886bfe85db463bb726346a29
Chudnovskyi <chudnovskiy0@gmail.com>
2023-02-25 20:39:46 +0100

    [Feature] tests for EmailValidator
    [Feature] tests for FieldMatchValidator

---
+++ src/test/java/com/myanimelist/validation/EmailValidatorTest.java
@@ -0,0 +1,111 @@
+package com.myanimelist.validation;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.lang.annotation.Annotation;
+import java.util.stream.Stream;
+
+import javax.validation.Payload;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class EmailValidatorTest {
+
+	@InjectMocks
+	private EmailValidator emailValidator;
+
+	@ParameterizedTest(name = "{index} => email={0}, domain={1}, isValid={2}")
+	@MethodSource(value = {
+			"noDomainEmails",
+			"singleDomainEmails",
+			"multipleDomainEmails"
+	})
+	void isEmailValid(String email, String[] domains, Boolean isValid) {
+		emailValidator.initialize(new EmailImpl(domains));
+
+		assertThat(emailValidator.isValid(email, null)).isEqualTo(isValid);
+	}
+
+	static Stream<Arguments> noDomainEmails() {
+		String[] domains = new String[] { "" };
+		return Stream.of(
+			Arguments.of("user@example.com", domains, true),
+			Arguments.of("user.name@example.com", domains, true),
+			Arguments.of("user123@example.com", domains, true),
+			Arguments.of("u_s_e_r@example.co.jp", domains, true),
+			Arguments.of("user@example.com.au", domains, true),
+			Arguments.of("user@subdomain.example.com", domains, true),
+			Arguments.of("user@127.0.0.1", domains, true),
+			Arguments.of("user@[127.0.0.1]", domains, true),
+			Arguments.of("user@", domains, false),
+			Arguments.of("user@localhost", domains, false),
+			Arguments.of("user@example", domains, false),
+			Arguments.of("user@.example.com", domains, false),
+			Arguments.of("user@-example.com", domains, false),
+			Arguments.of("user@example..com", domains, false)
+		);
+	}
+
+	static Stream<Arguments> singleDomainEmails() {
+		String[] domains = new String[] { "gmail.com" };
+		return Stream.of(
+			Arguments.of("user@gmail.com", domains, true),
+			Arguments.of("user.name@gmail.com", domains, true),
+			Arguments.of("user123@gmail.com", domains, true),
+			Arguments.of("u_s_e_r@gmail.co.jp", domains, false),
+			Arguments.of("user@gmail.com.au", domains, false),
+			Arguments.of("user@subdomain.gmail.com", domains, true),
+			Arguments.of("user@com", domains, false),
+			Arguments.of("user@mail.com", domains, false),
+			Arguments.of("@gmail.com", domains, false),
+			Arguments.of("gmail.comgmail.comgmail.com@gmail.com.gmail.com", domains, true),
+			Arguments.of("user@gmail.comm", domains, false)
+		);
+	}
+
+	static Stream<Arguments> multipleDomainEmails() {
+		String[] domains = new String[] { "gmail.com", "karazin.ua" };
+		return Stream.of(
+			Arguments.of("chudnovskyi_0@sub.karazin.ua", domains, true),
+			Arguments.of("chudnovskyi_0@sub.gmail.com", domains, true),
+			Arguments.of("chudnovskyi_0@sub.yahoo.com", domains, false),
+			Arguments.of("useerafds.fdssfd_sdfsfdr@exaasfd.karazin.ua", domains, true),
+			Arguments.of("", domains, false),
+			Arguments.of("@", domains, false),
+			Arguments.of("@example.co.jp", domains, false),
+			Arguments.of("sub.karazin.ua@sub.karazin.ua", domains, true),
+			Arguments.of("user@user", domains, false),
+			Arguments.of("user@karazin", domains, false),
+			Arguments.of("user@razin.ua", domains, false)
+		);
+	}
+
+	record EmailImpl(String[] domains) implements Email {
+
+		@Override
+		public String message() {
+			return null;
+		}
+
+		@Override
+		public Class<?>[] groups() {
+			return null;
+		}
+
+		@Override
+		public Class<? extends Payload>[] payload() {
+			return null;
+		}
+
+		@Override
+		public Class<? extends Annotation> annotationType() {
+			return null;
+		}
+	}
+}
---
+++ src/test/java/com/myanimelist/validation/FieldMatchValidatorTest.java
@@ -0,0 +1,82 @@
+package com.myanimelist.validation;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.lang.annotation.Annotation;
+import java.util.stream.Stream;
+
+import javax.validation.Payload;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class FieldMatchValidatorTest {
+	
+	@InjectMocks
+	private FieldMatchValidator fieldMatchValidator;
+
+	@ParameterizedTest(name = "{index} => ''{0}'' == ''{1}'' - true")
+	@MethodSource(value = "fieldsMatch")
+	void doFieldsMatch(String first, String second) {
+		fieldMatchValidator.initialize(new FieldMatchImpl("firstField", "secondField"));
+		
+		assertThat(fieldMatchValidator.isValid(new FieldMatchImpl(first, second), null)).isTrue();
+	}
+
+	static Stream<Arguments> fieldsMatch() {
+		return Stream.of(
+			Arguments.of("pass", "pass"),
+			Arguments.of("", ""),
+			Arguments.of(" ", " "),
+			Arguments.of(" 11", " 11"),
+			Arguments.of(" 11 ", " 11 ")
+		);
+	}
+	
+	@ParameterizedTest(name = "{index} => ''{0}'' = ''{1}'' - false")
+	@MethodSource(value = "fieldsNotMatch")
+	void doFieldsNotMatch(String first, String second) {
+		fieldMatchValidator.initialize(new FieldMatchImpl("firstField", "secondField"));
+		
+		assertThrows(NullPointerException.class, () -> fieldMatchValidator.isValid(new FieldMatchImpl(first, second), null));
+	}
+
+	static Stream<Arguments> fieldsNotMatch() {
+		return Stream.of(
+			Arguments.of("1", "2"),
+			Arguments.of("", " "),
+			Arguments.of("   ", "login"),
+			Arguments.of("password", "пароль"),
+			Arguments.of("i", "і") // english - ukrainian
+		);
+	}
+
+	record FieldMatchImpl(String firstField, String secondField) implements FieldMatch {
+
+		@Override
+		public String message() {
+			return null;
+		}
+
+		@Override
+		public Class<?>[] groups() {
+			return null;
+		}
+
+		@Override
+		public Class<? extends Payload>[] payload() {
+			return null;
+		}
+
+		@Override
+		public Class<? extends Annotation> annotationType() {
+			return null;
+		}
+	}
+}
